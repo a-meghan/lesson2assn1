c150778dbe86734fe4d40acd113137b79b80f6a3
a-meghan <a.meghanramsey@gmail.com>
2024-09-18 17:14:41 -0400

    first commit

---
+++ crossroad_decisions.py
@@ -0,0 +1,10 @@
+#Task 1- Code correction
+
+number = input("Enter a number: ")
+
+if int(number) > 0:
+    print("The number is positive.")
+elif int(number) == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
---
+++ greatest_showdown.py
@@ -0,0 +1,14 @@
+#Task 1- Identify the greatest
+
+x = int(input("Give me a number: "))
+y = int(input("Give another number: "))
+z = int(input("And one more... "))
+greatest = max(x,y,z)
+print(greatest)
+
+#Task 2- Identify the smallest
+a = int(input("Give me a number: "))
+b = int(input("Give another number: "))
+c = int(input("And one more... "))
+smallest = min(a,b,c)
+print(smallest)
